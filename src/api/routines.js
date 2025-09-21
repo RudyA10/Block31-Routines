68c7db2d57640eebcf85bb877108c9aa5f95a5d7
Rudy Acosta <rudyacosta323@gmail.com>
2025-09-21 12:40:44 -0500

    Adding API request files

---
+++ src/api/routines.js
@@ -0,0 +1,57 @@
+export async function getRoutines() {
+  try {
+    const response = await fetch(API + "/routines");
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
+
+export async function getRoutine(id) {
+  try {
+    const response = await fetch(API + "/routines/" + id);
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export async function createRoutine(token, routine) {
+  if (!token) {
+    throw Error("You must be signed in to create a routine.");
+  }
+
+  const response = await fetch(API + "/routines", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify(routine),
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
+
+export async function deleteRoutine(token, id) {
+  if (!token) {
+    throw Error("You must be signed in to delete a Routine.");
+  }
+
+  const response = await fetch(API + "/routines/" + id, {
+    method: "DELETE",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
